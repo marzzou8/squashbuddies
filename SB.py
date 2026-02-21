09d8d89c211ba077c67c877e507cb0fff36a2979
marzzou8 <marianizou@gmail.com>
2026-02-21 11:00:33 +0800

    Update SB.py

--- SB.py
+++ SB.py
@@ -26,7 +26,7 @@ def build_update_message(next_sunday, court_bookings, attendance_count, player_n
     # Court bookings
     if not court_bookings.empty:
         for _, row in court_bookings.iterrows():
-            message_lines.append(f" 📋 **Court:** {int(row['Court'])}  | **Time:** {row['Time Slot']}")
+            message_lines.append(f" 📋 **Court:** {int(row['Court'])}  | **Time:** {row['Time Slot']} | (Date {pd.to_datetime(unpaid_records.loc[i, 'Date']).strftime('%d %b %Y')")
     else:
         message_lines.append("No court bookings yet.")
 
@@ -299,6 +299,7 @@ st.write(f"💰 Current Balance: SGD {balance}")
 #    ])
 #    records.to_excel(excel_file, index=False)
 #    st.success("✅ Records have been reset. The app is now blank.")
+
 
 
 
